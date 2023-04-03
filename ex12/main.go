e338b55f130b7dc86ba360f8533cf91e901cd162
Дмитрий Остапчук <wisphes@gmail.com>
2023-04-03 22:41:06 +0300

    ex12

--- ex12/main.go
+++ ex12/main.go
@@ -10,13 +10,31 @@ func main() {
 		"cat",
 		"tree",
 	}
-	fmt.Println(RemoveDuplicates(input))
+	// Первый вариант решения без сохранения последовательности
+	fmt.Println(RemoveDuplicates1(input))
+	// Второй вариант решения с сохраненем последовательности
+	fmt.Println(RemoveDuplicates2(input))
 }
 
-func RemoveDuplicates(input []string) []string {
+func RemoveDuplicates1(items []string) []string {
 	m := make(map[string]int)
-	res := make([]string, 0, len(input))
-	for i, v := range input {
+	for _, v := range items {
+		m[v]++
+	}
+	/* здесь можно было вернуть сразу map
+	но так как я решил сделать независимыми два решения
+	пришлось результат поместить в слайс */
+	res := make([]string, 0, len(m))
+	for key := range m {
+		res = append(res, key)
+	}
+	return res
+}
+
+func RemoveDuplicates2(items []string) []string {
+	m := make(map[string]int)
+	res := make([]string, 0, len(items))
+	for i, v := range items {
 		if _, ok := m[v]; !ok {
 			res = append(res, v)
 		}
