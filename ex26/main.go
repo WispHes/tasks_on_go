740e80cafdfcca98a37a3e0e856fae42bd4cd14a
Дмитрий Остапчук <wisphes@gmail.com>
2023-04-03 21:22:44 +0300

    ex26

--- ex26/main.go
+++ ex26/main.go
@@ -1,5 +1,19 @@
 package main
 
-  func main() {
+import "fmt"
 
-  }
+func main() {
+	fmt.Println(uniqueSymbols("abCdefAaf"))
+}
+
+func uniqueSymbols(x string) bool {
+	m := make(map[rune]int)
+	for _, v := range x {
+		m[v]++
+		fmt.Println(v)
+		if m[v] > 1 {
+			return false
+		}
+	}
+	return true
+}
