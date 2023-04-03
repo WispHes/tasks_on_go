9a84eaea4af6b10633b5daf3fc01f35a5f529962
Дмитрий Остапчук <wisphes@gmail.com>
2023-04-03 20:44:31 +0300

    ex23

--- ex23/main.go
+++ ex23/main.go
@@ -1,5 +1,13 @@
 package main
 
-  func main() {
+import "fmt"
 
-  }
+func main() {
+	i, s := 3, []int{2, 3, 4, 2, 1, 5, 6}
+	fmt.Println(elementDeleting(s, i))
+}
+
+func elementDeleting(s []int, i int) []int {
+	s = append(s[:i], s[i+1:]...)
+	return s
+}
