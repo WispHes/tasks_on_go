e76b9f9e0c081cd50f78a922aa19d9c1b201bd08
Дмитрий Остапчук <wisphes@gmail.com>
2023-04-03 23:50:41 +0300

    ex19

--- ex19/main.go
+++ ex19/main.go
@@ -1,5 +1,16 @@
 package main
 
-  func main() {
+import "fmt"
 
-  }
+func main() {
+	fmt.Println(flippingLine("главрыба"))
+}
+
+func flippingLine(items string) string {
+	s := []rune(items)
+	n := len(s) - 1
+	for i := 0; i <= n/2; i++ {
+		s[i], s[n-i] = s[n-i], s[i]
+	}
+	return string(s)
+}
