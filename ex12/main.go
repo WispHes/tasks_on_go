53e1d0613931d0d221f885f1cfa967bc57b2f399
Дмитрий Остапчук <wisphes@gmail.com>
2023-04-03 19:36:55 +0300

    ex12

--- ex12/main.go
+++ ex12/main.go
@@ -1,5 +1,26 @@
 package main
 
-  func main() {
+import "fmt"
 
-  }
+func main() {
+	input := []string{
+		"cat",
+		"cat",
+		"dog",
+		"cat",
+		"tree",
+	}
+	fmt.Println(RemoveDuplicates(input))
+}
+
+func RemoveDuplicates(input []string) []string {
+	m := make(map[string]int)
+	res := make([]string, 0, len(input))
+	for i, v := range input {
+		if _, ok := m[v]; !ok {
+			res = append(res, v)
+		}
+		m[v] = i
+	}
+	return res
+}
